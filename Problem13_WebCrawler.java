70d9b3aa7a446600cb64931cb6e25e276baa97e2
https://github.com/Icakis <Icakis87@gmail.com>
2014-06-03 22:48:12 +0300

    new commit Git

--- Problem13_WebCrawler.java
+++ Problem13_WebCrawler.java
@@ -106,4 +106,5 @@ class Problem13_WebCrawler {
 		writeFile(allURLonPage);
 	}
 }
-//made some changes with tortoisesvn client and i'll come it 
+//made some changes with tortoisesvn client and i'll commit it 
+//made some changes and commit it with GitBash
